d38f3ad2fae01fd82af10ef5949f67586c1a2e54
johnniang <johnniang@riseup.net>
2019-05-16 20:31:25 +0800

    Complete A1011 algorithm

---
+++ A1011/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char result[3] = {'W', 'T', 'L'};
+    float number[3] = {0};
+    float max[3] = {0};
+    float total = 1.0;
+    for(int i = 0; i < 3; i++) {
+        scanf("%f %f %f", &number[0], &number[1], &number[2]);
+        int maxIdx = 0;
+        for(int j = 0; j < 3; j++) {
+            if(number[j] > max[i]) {
+                max[i] = number[j];
+                maxIdx = j;
+            }
+        }
+        printf("%c ", result[maxIdx]);
+
+        total *= max[i];
+    }
+
+    // Calculate the result
+    printf("%.2f", (total * 0.65 - 1) * 2);
+    return 0;
+}
