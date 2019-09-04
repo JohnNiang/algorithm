5246e8ba5c403ef5005612180cf7ffa13df7d06a
johnniang <johnniang@foxmail.com>
2019-09-04 21:03:06 +0800

    Remove unused segment

--- A1051/main.cpp
+++ A1051/main.cpp
@@ -6,18 +6,7 @@ using namespace std;
 // A1051: Pop Sequence
 // <https://pintia.cn/problem-sets/994805342720868352/problems/994805427332562944>
 
-void reverse(int* numbers, int len)
-{
-    for(int i = 0; i < len / 2; i++)
-    {
-        int temp = numbers[i];
-        numbers[i] = numbers[len - i - 1];
-        numbers[len - i - 1] = temp;
-    }
-}
-
-int main()
-{
+int main() {
     int M = 0;
     int N = 0;
     int K = 0;
@@ -25,43 +14,32 @@ int main()
 
     int numbers[1000];
 
-    for(int k = 0; k < K; k++)
-    {
-        for(int n = 0; n < N; n++)
-        {
+    for(int k = 0; k < K; k++) {
+        for(int n = 0; n < N; n++) {
             cin >> numbers[n];
         }
-        // Reverse numbers
-        // reverse(numbers, N);
 
         int n = N;
 
         stack<int> s;
         bool exceed = false;
-        for(int i = N - 1; i >= 0; i--)
-        {
-            if(s.size() == M)
-            {
+        for(int i = N - 1; i >= 0; i--) {
+            if(s.size() == M) {
                 exceed = true;
                 break;
             }
 
             s.push(numbers[i]);
 
-            while(!s.empty() && s.top() == n)
-            {
+            while(!s.empty() && s.top() == n) {
                 s.pop();
                 n--;
             }
-
         }
 
-        if(!exceed && s.empty())
-        {
+        if(!exceed && s.empty()) {
             cout << "YES" << endl;
-        }
-        else
-        {
+        } else {
             cout << "NO" << endl;
         }
     }
